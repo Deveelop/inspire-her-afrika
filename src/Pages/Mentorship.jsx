f07d47f958c5acc626f3b65312c247aa8de2aea6
Victor Abuka <vicsonabuka@gmail.com>
2024-02-11 23:58:04 +0100

    Added new changes

--- src/Pages/Mentorship.jsx
+++ src/Pages/Mentorship.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 function Mentorship() {
   return (
-    <>
-    <div className="w-[100%] h-[] bg-yellow-50">
+    <div className=' '>
+    <div className="w-[100%] bg-yellow-50 ">
 
     <div className=' mb-20 bg-[url("/groupy.png")] h-[555px] md:h-2/3 w-full bg-cover
         bg-center p-4 md:p-72 relative
         '>
         <div className=' absolute top-60 flex justify-center'>
           <div className=' grid gap-3 '>
-          <div className="text-justify text-white text-[40px] font-bold font-Athiti leading-[37.31px]">Mentorship Program</div>
-          <div className="w-[] h-[59px] text-justify text-yellow-950 text-[64px] font-bold font-['Athiti'] leading-[37.31px]">We’ve currated a structured </div>
+          <div className=" md:text-justify text-white md:text-[40px] text-2xl font-bold font-Athiti leading-[37.31px]">Mentorship Program</div>
+          <div className="w-[] h-[59px] text-justify text-yellow-950 md:text-[64px] text-2xl font-bold font-['Athiti'] leading-[37.31px]">We’ve currated a structured </div>
     
       <div className=''>
         <button className='bg-text-white font-semibold bg-white p-3 w-32 rounded-xl' >Join Us</button>
@@ -20,74 +20,77 @@ function Mentorship() {
          </div>
          </div>
 
-    <div className=' pb-8 '>
-    <div className=" ">
-        <h1 className="">What we Offer</h1>
-        <div className=" " />
+    <div className=' pb-8 m-auto w-[80%] '>
+    <div className=" md:h-[384px] items-center flex flex-col justify-center leading-9  ">
+        <h1 className=" text-center font-Athiti font-bold md:text-[64px] text-4xl leading-[46px]">What we Offer</h1>
+        <div className=" w-[80%] md:w-[326px] h-[0px] m-10 border-[5px] border-[#36200F] " />
 
-        <div className="">The Inspire Her Afrika Mentorship Program is a transformative and empowering initiative designed to uplift and guide young African women on their journey to personal and professional success. Spanning six months, the program facilitates one-on-one connections between experienced mentors and ambitious mentees, fostering a dynamic learning environment.<br/>The best part is the mentorship pairs—each mentee gets a mentor. It's like having a friend who has been there, done that, and is ready to help you. The program operates on the premise that mentorship is a two-way exchange, where mentors find fulfilment in guiding, and mentees unearth their potential.<br/>
+        <div className=" text-[#36200F]  text-center md:text-[32px] font-normal;
+ ">The Inspire Her Afrika Mentorship Program is a transformative and empowering initiative designed to uplift and guide young African women on their journey to personal and professional success. Spanning six months, the program facilitates one-on-one connections between experienced mentors and ambitious mentees, fostering a dynamic learning environment.<br/>The best part is the mentorship pairs—each mentee gets a mentor. It's like having a friend who has been there, done that, and is ready to help you. The program operates on the premise that mentorship is a two-way exchange, where mentors find fulfilment in guiding, and mentees unearth their potential.<br/>
         </div>
     </div>
     </div>
 
-    <div className=" ">
-        <div className="">
-            <h1 className="">What to Expect</h1>
-            <div className=" " />
+    <div className=" m-auto w-[80%] ">
+        <div className=" md:h-[384px] items-center flex flex-col justify-center leading-9">
+            <h1 className=" text-center font-Athiti font-bold md:text-[64px] text-3xl leading-[46px]">What to Expect</h1>
+            <div className=" w-[80%] md:w-[326px] h-[0px] m-10 border-[5px] border-[#36200F] " />
         </div>
-        <div className="">
-            <div className=" grid grid-cols-2 ">
+        <div className=" ">
+            <div className=" grid md:grid-cols-2 items-center gap-10 mb-20">
                 <div>
-                <img className="w-[482px] h-[373px] rounded-[10px]" src="https://via.placeholder.com/482x373" />
+                <img className=" w-[482px] md:h-[373px] rounded-[10px]" src="view3.png" />
                 </div>
                 <div className=" ">
-                    <div className=" "><h5 className="">Structured Curriculum:<br/></h5>
-                    <p className="">The program employs a carefully curated curriculum, addressing critical areas such as goal-setting, personal branding, career development, and project execution. This ensures a comprehensive and impactful learning experience.</p>
+                    <div className="">
+                        <h5 className="md:text-[40px] text-xl font-semibold mb-5 ">Structured Curriculum:<br/></h5>
+                    <p className=" md:text-[24px] text-sm font-normal md:leading-10">The program employs a carefully curated curriculum, addressing critical areas such as goal-setting, personal branding, career development, and project execution. This ensures a comprehensive and impactful learning experience.</p>
                     </div>
                 </div>
             </div>
 
-            <div className="grid grid-cols-2">
+            <div className="grid md:grid-cols-2 items-center gap-10 mb-20">
                 <div className="">
-                <h5 className="">Active learning:<br/></h5>
-                <p className="">Mentees are not passive learners but active participants in their growth. Each month, mentors and mentees will receive briefs outlining their focus for the month and providing resources to guide the completion of tasks tailored to enhance mentee’s skills, confidence, and problem-solving abilities.</p>
+                <h5 className=" md:text-[40px] text-xl  font-semibold mb-5">Active learning:<br/></h5>
+                <p className=" md:text-[24px] text-sm font-normal md:leading-10 ">Mentees are not passive learners but active participants in their growth. Each month, mentors and mentees will receive briefs outlining their focus for the month and providing resources to guide the completion of tasks tailored to enhance mentee’s skills, confidence, and problem-solving abilities.</p>
                 </div>
                 <div>
-                <img className="w-[437px] h-[328px] rounded-[10px]" src="https://via.placeholder.com/437x328" />
+                <img className="w-[482px] md:h-[373px] rounded-[10px]" src="view4.png" />
                 </div>
             </div>
 
-            <div className="grid grid-cols-2">
+            <div className="grid md:grid-cols-2 items-center gap-10 mb-20">
                 <div className=''>
-                <img className="w-[547.53px] h-[373px] rounded-[10px]" src="https://via.placeholder.com/548x373" />
+                <img className="w-[482px] md:h-[373px] rounded-[10px]" src="view.png" />
                 </div>
-                <div className="w-[579px] text-justify"><h5 className="text-yellow-950 text-[40px] font-semibold font-['Athiti'] leading-[45.60px]">Reflection and Evaluation:<br/></h5>
-                <p className="text-yellow-950 text-2xl font-normal font-['Athiti'] leading-[38px]">The program places a strong emphasis on self-reflection. Through weekly meetings, the accompanied log submissions and surveys, mentees (and the program team) are able to track progress, receive feedback, and refine their strategies.</p>
+                <div className="">
+                    <h5 className="md:text-[40px] text-xl  font-semibold mb-2">Reflection and Evaluation:<br/></h5>
+                <p className=" md:text-[24px] text-sm font-normal md:leading-10">The program places a strong emphasis on self-reflection. Through weekly meetings, the accompanied log submissions and surveys, mentees (and the program team) are able to track progress, receive feedback, and refine their strategies.</p>
                 </div>
             </div>
-            <div className="grid grid-cols-2">
-                <div className="w-[593px]">
-                    <h5 className="">Community Engagement:<br/></h5>
-                    <p className="">Beyond individual growth, the program encourages mentees to actively participate in community-oriented projects, fostering a sense of social responsibility and leadership.</p>
+            <div className="grid md:grid-cols-2 items-center gap-10 mb-20">
+                <div className="">
+                    <h5 className="md:text-[40px] text-xl  font-semibold mb-2">Community Engagement:<br/></h5>
+                    <p className=" md:text-[24px] text-sm font-normal md:leading-10">Beyond individual growth, the program encourages mentees to actively participate in community-oriented projects, fostering a sense of social responsibility and leadership.</p>
                     </div>
                     <div>
-                <img className="w-[543px] h-[350px] rounded-[10px]" src="https://via.placeholder.com/543x350" />
+                <img className="w-[482px] md:h-[373px] rounded-[10px]" src="view1.png" />
                  </div>
             </div>
-            <div className=" grid grid-cols-2">
+            <div className=" mb grid md:grid-cols-2 items-center gap-10 pb-4">
                 <div>
-                <img className="w-[539px] h-[373px] rounded-[10px]" src="https://via.placeholder.com/539x373" />
+                <img className="w-[482px] md:h-[373px] rounded-[10px]" src="view2.png" />
                 </div>
 
                 <div className="">
-                    <h5 className="">Networking Opportunities:<br/></h5>
-                    <p className="">The inclusion of informational interviews, mentor consultations and virtual mastery sessions facilitated by subject experts provides mentees with networking opportunities that extend beyond their immediate mentorship circle.</p>
+                    <h5 className="md:text-[40px] text-xl  font-semibold mb-2">Networking Opportunities:<br/></h5>
+                    <p className=" md:text-[24px] text-sm font-normal md:leading-10">The inclusion of informational interviews, mentor consultations and virtual mastery sessions facilitated by subject experts provides mentees with networking opportunities that extend beyond their immediate mentorship circle.</p>
                     </div>
             </div>
         </div>
     </div>
 </div>
-    </>
+    </div>
   )
 }
 
