414bbfac3047d60345761954e706df3f592f4bfe
Victor Abuka <vicsonabuka@gmail.com>
2024-02-26 16:22:10 +0100

    Added new changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,19 +4,19 @@ import { BsInstagram, BsLinkedin, BsTwitter } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <footer className="grid md:grid-cols-3 bg-orange-300 text-white md:p-8 p-4">
+    <footer className="md:grid md:grid-cols-3 bg-orange-300 text-white md:p-8 p-4">
 
-      <div className="flex flex-col col-span-3 md:col-span-1 py-2">
-            <Logo />
+      <div className="flex flex-col py-2 w-[300px]">
+            <img src='/logo.png' alt='all' />
             <p className="text-yellow-950">
             We are committed to raising the next generation of female African leaders by delivering a comprehensive mentorship program designed to provide personalised guidance and support to African women who are seeking to improve their skills, knowledge, and career prospects.
             </p>
       </div>
 
-      <figure className="grid md:grid-cols-2 grid-cols-1 col-span-3 md:col-span-1 py-2">
-        <div className="text-yellow-950">
-          <h1 className="text-yellow-950 font-bold text-2xl"> About Us</h1>
-            <div className="pl-3 my-2">
+      <figure className=" md:flex  gap-10 ">
+        <div className="text-yellow-950 whitespace-nowrap">
+          <h1 className="text-yellow-950 font-bold text-xl"> About Us</h1>
+            <div className=" my-2">
               <p>  Our Mission </p>
               <p>  Our Vision </p>
               <p>The Team </p>
@@ -24,9 +24,9 @@ const Footer = () => {
             </div>
         </div>
         
-        <div className="text-yellow-950">
-          <h1 className="text-yellow-950 font-bold text-2xl"> Quick Links </h1>
-            <div className="pl-3 my-2">
+        <div className="text-yellow-950 whitespace-nowrap">
+          <h1 className="text-yellow-950 font-bold text-xl"> Quick Links </h1>
+            <div className=" my-2">
               <p>Become a Volunteer </p>
               <p>Contact Us </p>
               <p> Event </p>
@@ -34,9 +34,9 @@ const Footer = () => {
             </div>
         </div>
 
-        <div className="text-yellow-950 ">
-          <h1 className="text-yellow-950 font-bold text-2xl"> Useful Links </h1>
-            <div className="pl-3 my-2">
+        <div className="text-yellow-950 whitespace-nowrap ">
+          <h1 className="text-yellow-950 font-bold text-xl"> Useful Links </h1>
+            <div className="my-2">
               <p>Blog </p>
               <p>Testimonias</p>
             </div>
@@ -44,7 +44,7 @@ const Footer = () => {
        
       </figure>
 
-      <div className="  flex-col justify-start items-start gap-5 inline-flex">
+      <div className="  flex-col justify-start items-start gap-5 inline-flex ml-10 py-1">
                 <div className="text-yellow-950 text-lg font-semibold font-Athiti  leading-snug">Join Our Newsletter</div>
                 <form className=" md:bg-white  md:w-[365px] h-[302px] left-0 top-0 relative rounded-tl-[20px] rounded-bl-[20px] rounded-br-[20px] border border-yellow-950">                 
                      <div className="left-[22px] top-[42px] absolute flex-col justify-start items-start gap-4 inline-flex">
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -8,9 +8,11 @@ const Home = () => {
     
      <div className=' '>
       
-       <div className=' mb-20 bg-[url("/groupy.png")] h-[555px] md:h-2/3 w-full bg-cover
-        bg-center p-4 md:p-72 relative
+       <div className=' mb-20 h-screen w-full 
+         p-4 md:p-72 relative
         '>
+          <div className='absolute inset-0 bg-[url("groupshot.png")] bg-cover bg-center'/>
+          <div className=' absolute inset-0 bg-gradient-to-r from-[#D6A530] via-[#D6AD4BD7]  to-transparent opacity-90 w-full' />
         <div className=' absolute top-60 flex justify-center'>
           <div className=' grid gap-3 '>
         <h1 className='text-[#361F0F] text-2xl font-Athiti md:text-6xl'>Inspire Her Afrika</h1>
@@ -24,30 +26,30 @@ const Home = () => {
         
         <div className=' w-[80%] m-auto flex flex-col justify-center'>
          <div className=" mb-20  backdrop-blur-sm flex-col justify-center items-center gap-10 inline-flex">
-    <div className="text-justify text-yellow-950 md:text-[64px] font-bold font-['Athiti'] leading-[45.60px]">
+    <div className="text-justify text-yellow-950 md:text-xl font-bold border-b-4 border-yellow-950">
       <h2>Who we are</h2>
     </div>
-    <div className="w-[251px]  border-4 border-yellow-950"></div>
-    <div className=" p-4 md:w-[1099px] bg-white rounded-[20px] shadow justify-center items-center gap-2 inline-flex">
-        <p className=" text-center text-yellow-950 md:text-2xl  leading-[36px]"><br/>We are all about the African woman<br/>We are committed to raising the next generation of female African leaders by delivering a comprehensive mentorship program designed to provide personalised guidance and support to African women who are seeking to improve their skills, knowledge, and career prospects.<br/></p>
+ 
+    <div className=" -mt-5 p-4 md:w-[1099px] bg-white rounded-[20px] shadow-md justify-center items-center gap-2 inline-flex">
+        <p className=" text-center text-yellow-950 md:text-lg"><br/>We are all about the African woman<br/>We are committed to raising the next generation of female African leaders by delivering a comprehensive mentorship program designed to provide personalised guidance and support to African women who are seeking to improve their skills, knowledge, and career prospects.<br/></p>
     </div>
 
-    <div className=" md:w-[1099px] bg-white rounded-[20px] shadow text-center">
-      <span className="text-yellow-950 md:text-4xl font-bold leading-[37.31px]">Our Vision<br/></span>
-      <p className=" p-4 text-yellow-950 md:text-2xl  leading-[37.31px]">To provide thorough mentorship, guidance, and networking opportunities that will inspire African women to pursue their passions, develop their skills, and become leaders in their professional spaces. We aim to create a safe community for African women to connect, learn, grow and encourage themselves to achieve their full potential. We aspire to be the leading mentorship group for African women looking to advance professionally.</p>
+    <div className=" md:w-[1099px] bg-white rounded-[20px] shadow-md text-center">
+      <h1 className="text-yellow-950 md:text-xl font-bold pt-4 -mb-4 ">Our Vision</h1>
+      <p className=" p-4 text-yellow-950 md:text-lg ">To provide thorough mentorship, guidance, and networking opportunities that will inspire African women to pursue their passions, develop their skills, and become leaders in their professional spaces. We aim to create a safe community for African women to connect, learn, grow and encourage themselves to achieve their full potential. We aspire to be the leading mentorship group for African women looking to advance professionally.</p>
       </div>
 
 
 <div className=" md:w-[1099px] bg-white  rounded-[20px] shadow text-center">
-  <span className="text-yellow-950 md:text-4xl font-bold font-['Athiti'] leading-[37.31px]">Our Mission<br/></span>
-  <p className=" p-4 text-yellow-950 md:text-2xl leading-[37.31px]">Connecting Women To create a safe community for African women to connect, learn, grow and encourage themselves to achieve their full potential.</p>
+  <h1 className="text-yellow-950 md:text-xl font-bold pt-4 -mb-4">Our Mission</h1>
+  <p className=" p-4 text-yellow-950 md:text-lg ">Connecting Women To create a safe community for African women to connect, learn, grow and encourage themselves to achieve their full potential.</p>
   </div>
 </div>
 </div>
 
 <div className='relative'>
-<div className="  bg-[url('/groupy.png')] bg-black bg-opacity-75 p-10 md:p-40  w-full bg-cover" >
-<div className='absolute inset-0 bg-black bg-opacity-75'></div>
+<div className="  bg-[url('/unsplash.png')] bg-black bg-opacity-75 p-10 md:p-40  w-full bg-cover" >
+<div className='absolute inset-0 bg-black bg-opacity-75'/>
 <div className="w-[392px]  flex-col justify-start items-start gap-[62px] inline-flex relative">
     <div className="flex-col justify-start items-start gap-3.5 flex">
         <p className="text-center text-neutral-100 md:text-[32px] font-bold font-['Athiti'] leading-[62px]">8 African Countries</p>
