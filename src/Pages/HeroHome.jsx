024267e9d3a063ce68cf4b83d2824ffc0917c120
Victor Abuka <vicsonabuka@gmail.com>
2024-02-27 09:30:01 +0100

    Added new chnages

--- src/Pages/HeroHome.jsx
+++ src/Pages/HeroHome.jsx
@@ -5,7 +5,7 @@ const HeroHome = () => {
     
     <div className=' mb-20  h-[400px] md:w-full p-4 md:p-72 relative '>
     <div className=" absolute inset-0 bg-[url('groupshot.png')] md:bg-cover bg-contain bg-no-repeat bg-center bg-opacity-90"/>
-      <div className=' absolute inset-0 bg-gradient-to-r from-[#D6A530] via-[#D6AD4BD7]  to-transparent opacity-20 w-full' />
+      <div className='' />
     <div className=' absolute top-60 flex justify-center'>
       <div className=' grid gap-3 '>
     <h1 className='text-[#361F0F] text-2xl font-Athiti md:text-6xl'>Inspire Her Afrika</h1>
